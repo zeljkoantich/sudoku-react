bba4706649b270f4f2ebc1c6ce8a879d0b26e93f
zeljko_a <zeljko.antich@gmail.com>
2020-05-17 15:58:57 +0200

    use React.Children.toArray method for assignin unique component keys

--- src/components/grid/grid.component.tsx
+++ src/components/grid/grid.component.tsx
@@ -1,23 +1,31 @@
-import React, { FC } from 'react';
+import React, { FC, Children } from 'react';
 import { CyDataAttributes as cy } from '../../common';
 
 const GridComponent: FC = () => {
   const rows = new Array(9);
   return (
     <div data-cy={cy.GRID_CONTAINER}>
-      { [...rows].map((_, rowIndex) => GRID_ROW(rowIndex)) }
+      {
+        Children.toArray( // for unique keys (instead of array index)
+          [...rows].map(() => GRID_ROW())
+        )
+      }
     </div>
   )
 };
-const GRID_ROW = (id: number) => {
+const GRID_ROW = () => {
   const blocks = new Array(9);
   return (
-    <div data-cy={cy.GRID_ROW_CONTAINER} key={id}>row
-      { [...blocks].map((_, columIndex) => GRID_BLOCK(columIndex)) }
+    <div data-cy={cy.GRID_ROW_CONTAINER}>row
+      {
+        Children.toArray( // for unique keys (instead of array index)
+          [...blocks].map(() => GRID_BLOCK())
+        )
+      }
     </div>
   );
 };
 
-const GRID_BLOCK = (id: number) => <div data-cy={cy.BLOCK} key={id}>block</div>;
+const GRID_BLOCK = () => <div data-cy={cy.BLOCK}>block</div>;
 
 export default GridComponent;
